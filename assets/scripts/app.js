af8bd23b9422552456dcee2f44dd4cdbf1b837a4
Ralph van Schaik <ralph.van.schaik@me.com>
2020-10-21 15:17:33 +0200

    Tm 38

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,16 @@ const defaultResult = 0;
 
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return parseInt(usrInput.value);
+}
+
 function add() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`
     currentResult = currentResult + parseInt(userInput.value);
     //+userInput.value cast het ook.
-    outputResult(currentResult, '');
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
